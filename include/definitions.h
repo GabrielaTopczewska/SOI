9095f9c3424ca73349c8b7046741f3e6a9eb9ab0
Gabriela Topczewska <01158963@pw.edu.pl>
2021-12-11 17:39:44 +0100

    Upload New File

---
+++ include/definitions.h
@@ -0,0 +1,25 @@
+#ifndef DEF_H
+#define DEF_H
+
+#define N 10
+#define BUFS 3
+#define BUF1 0
+#define BUF2 1
+#define BUF3 2
+
+#define PROCS 5
+#define P1 0
+#define P2 1
+#define C1 2
+#define C2 3
+#define C3 4
+
+#define BUF1 0
+#define BUF2 1
+#define BUF3 2
+
+#define SEM 0
+#define EMPTY 1
+#define FULL 2
+
+#endif
