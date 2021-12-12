ce4769815e4e3a21a7edec1ecf4f5c99da7f89ab
Gabriela Topczewska <gabriela.topczewska.stud@pw.edu.pl>
2021-12-12 11:04:53 +0100

    hindus

---
+++ te_soi_zajebane/definitions.h
@@ -0,0 +1,22 @@
+#ifndef DEF_H
+#define DEF_H
+
+#define N 10
+#define MAX_DATA 10
+
+#define PROCS 5
+#define P1 0
+#define P2 1
+#define C1 2
+#define C2 3
+#define C3 4
+
+#define BUF1 0
+#define BUF2 1
+#define BUF3 2
+
+#define FULL_VAL 0
+#define MUT_VAL 1
+#define PSHARED 1
+
+#endif
