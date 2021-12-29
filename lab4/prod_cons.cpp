958b979fc1568a923c3a0fd7f2a261d1de8b0405
Gabriela Topczewska <gabriela.topczewska.stud@pw.edu.pl>
2021-12-29 17:05:10 +0100

    rand int

--- lab4/prod_cons.cpp
+++ lab4/prod_cons.cpp
@@ -1,2 +1,19 @@
 #include <prod_cons.h>
 
+int producer_a(void)
+{
+    int value = rand() % 1000;
+
+    while(value % 2 == 0) { value = rand() % 1000; }
+
+    return value;
+}
+
+int producer_b(void)
+{
+    int value = rand() % 1000;
+
+    while(value % 2 != 0) { value = rand() % 1000; }
+
+    return value;
+}
--- lab4/prod_cons.h
+++ lab4/prod_cons.h
@@ -1,5 +1,9 @@
 #ifndef PROD_CONS_H
 #define PROF_CONS_H
 
+#include <stdlib.h>
+
+int producer_a(void);
+int producer_b(void);
 
 #endif
